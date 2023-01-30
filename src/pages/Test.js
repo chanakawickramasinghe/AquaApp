4f0a367cb06f26cd7a1baf930d33ae05bc453d7d
AnushkaDarshana <2018is087@stu.ucsc.cmb.ac.lk>
2023-01-30 13:00:41 +0530

    firebase

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,13 +1,31 @@
-import React,{useRef} from 'react'
+import React,{useRef} from 'react';
+import {addDoc,collection } from "@firebase/firestore";
+import {firestore} from "../firebase";
 
 export default function Test(){
 
     const messageRef = useRef(null);
+    const ref = collection (firestore,"messages"); 
+
+    const handleSave = async (e) => {
+        e.preventDefault();
+        console.log(messageRef.current.value);
+
+        const data = {
+            message: messageRef.current.value,
+        }
+
+        try{
+            addDoc(ref,data);
+        }catch(e){
+            console.log(e);
+        }
+    };
+
     return(
-            <form>
-                <label htmlFor="name">Enter Name</label>
-                <input type="text" id="name" ref={messageRef} />
-                <button type="submit">Save</button>
-            </form>
+        <form onSubmit={handleSave}>
+            <input type="text" id="name" ref={messageRef} />
+            <button type="submit">Save</button>
+        </form>
     )
 }
